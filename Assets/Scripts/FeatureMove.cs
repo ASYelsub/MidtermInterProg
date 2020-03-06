239cfd9a34177e46761e81f50189178f85bfe95e
Abigail <asyelsub@gmail.com>
2020-03-06 14:12:12 -0500

    First interaction done

---
+++ Assets/Scripts/FeatureMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatureMove : MonoBehaviour
+{
+    public GameObject target;
+    Vector2 targetPos;
+    Vector2 myTargetPos;
+    float lerpNumber;
+    void Start()
+    {
+        myTargetPos = this.transform.position;
+    }
+
+    void Update()
+    {
+        target.transform.position = targetPos;
+        Debug.Log(targetPos);
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnMouseDown();
+        }
+        transform.position = Vector3.Lerp(transform.position, myTargetPos, lerpNumber);
+    }
+
+    private void OnMouseDown()
+    {
+        lerpNumber = Random.Range(0f, .5f);
+        targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        myTargetPos = Vector3.Lerp(transform.position, targetPos, lerpNumber);
+    }
+
+
+}
