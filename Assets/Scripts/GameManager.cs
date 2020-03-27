661fbe0870c2f3fbb2f5e2158af9cc942ec17fdb
Abigail <asyelsub@gmail.com>
2020-03-27 01:32:57 -0400

    cleaned up files and added out-of-game screen/beginning screen/introduction.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void StartGame() //I don't know if UI buttons & functions were covered in class.
+    {
+        print("hello");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
